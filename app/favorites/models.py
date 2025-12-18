9e116a87c155d9d1a5bb2467bcf83c1440f41a8c
Artem2335 <art20072975@gmail.com>
2025-12-18 12:29:12 +0300

    Fix Favorite model - add ForeignKey constraints

--- app/favorites/models.py
+++ app/favorites/models.py
@@ -1,11 +1,12 @@
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base, int_pk
 
 
 class Favorite(Base):
     id: Mapped[int_pk]
-    movie_id: Mapped[int] = mapped_column(nullable=False)
-    user_id: Mapped[int] = mapped_column(nullable=False)
+    movie_id: Mapped[int] = mapped_column(ForeignKey('movies.id'), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
 
     # Relationships
     movie: Mapped["Movie"] = relationship("Movie", back_populates="favorites", foreign_keys=[movie_id])
